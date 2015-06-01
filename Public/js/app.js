4c50a07d0449175657ff502eb74a02c095d321a2
Hollis Lau <hollislau@hotmail.com>
2015-06-01 12:37:27 -0700

    Restucture repo directory and add scaffolding for js file

---
+++ Public/js/app.js
@@ -0,0 +1,39 @@
+$(function() {
+
+  var User = {
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    location: "",
+    phoneNumber: "",
+
+    createAccount: function() {
+
+    },
+
+    loadApp: function() {
+
+    }
+  }
+
+  var Request = {
+    requestorID: "",
+    description: "",
+    isActive: "",
+    date: "",
+
+    createRequest: function() {
+
+    },
+
+    respondRequest: function() {
+
+    },
+
+    deactivateRequest: function() {
+
+    }
+  }
+
+});
