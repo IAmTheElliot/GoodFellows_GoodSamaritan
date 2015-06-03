5c6b1434729a1aa55c385234351768049fa8b5f8
Elliot T Simpson <Falcon5783@aol.com>
2015-06-03 11:40:40 -0700

    commit for sync from online master

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  function User() {
+  function UserProfile() {
     this.email = "";
     this.password = "";
     this.firstName = "";
@@ -10,17 +10,30 @@ $(function() {
     this.id = "";
   }
 
-  var user = new User();
+  var user = new UserProfile();
 
-  User.prototype.createAccount = function() {
-
+// Fetches values from newuser.html and pushe them to Firebase to create a new user account
+  UserProfile.prototype.createAccount = function() {
+    var newUserURL = new Firebase('https://good-samaritan-cf.firebaseio.com/User');
+    newUserURL.push({
+      email: $('#new-email').val(),
+      password: $('#new-password1').val(),
+      firstName: $('#new-firstname').val(),
+      lastName: $('#new-lastname').val(),
+      phoneNumber: $('#new-phonenumber').val(),
+      city: $('#new-city').val(),
+      state: $('#new-state :selected').val(),
+      zip: $('#new-zipcode').val()
+    }, user.signIn($('#new-email').val(), $('#new-password1').val()));
   };
 
-  User.prototype.renderUserInfo = function(info) {
+
+
+  UserProfile.prototype.renderUserInfo = function(info) {
     console.log(info);
   };
 
-  User.prototype.signIn = function(email, password) {
+  UserProfile.prototype.signIn = function(email, password) {
     var self = this;
 
     var userDataRef = new Firebase("https://good-samaritan-cf.firebaseio.com/User");
@@ -47,7 +60,7 @@ $(function() {
   }
 
   function Request() {
-    this.requestorID = "";
+    this.requestorId = "";
     this.description = "";
     this.isActive = "";
     this.date = "";
@@ -57,9 +70,29 @@ $(function() {
 
   };
 
+// Creates a new request for assistance
+// MISSING jQUERY REFERENCES TO HTML/DOM
+// CLICK EVENT BELOW NEEDS POINTER
   Request.prototype.createRequest = function() {
-
+    newUserRequest = new Firebase('https://good-samaritan-cf.firebaseio.com/Request');
+    newUserRequest.push({
+      requestorId: "test id goes here",
+      description: "$('#requestorIDhere').val()",
+      isActive: true,
+      date: event.timeStamp
+    }, console.log("createRequest has been pushed to firebase"))
   };
+
+// saved sample code for using moment() to return timestamp to viewable date
+/*  $('#test-button').on('click', function(){
+    var dateVar = event.timeStamp;
+    console.log(event.timeStamp);
+    console.log(moment());
+    console.log(moment(1433292533519));
+    console.log(moment(dateVar));
+  })
+*/
+
 
   Request.prototype.respondRequest = function() {
 
@@ -74,5 +107,10 @@ $(function() {
     var password = $("input#password").val();
     user.signIn(email, password);
   })
+
+  $('#new-user-form').on('submit', user.createAccount);
+
+  // $('#CREATENEWREQUESTNAMEHERE').on('submit', user.createRequest);
+  $('#test-button').on('click', Request.createRequest);
 
 });
