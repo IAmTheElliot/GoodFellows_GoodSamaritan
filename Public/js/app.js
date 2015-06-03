43cc0a15a19bb4ee2a30bef68573adafe6ec05ec
Elliot T Simpson <Falcon5783@aol.com>
2015-06-03 14:26:07 -0700

    primary sync for Wednesday

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,19 +1,22 @@
 $(function() {
 
-  function UserProfile() {
+  function userProfile() {
     this.email = "";
     this.password = "";
     this.firstName = "";
     this.lastName = "";
-    this.location = "";
+    this.city = "";
+    this.state = "";
+    this.zip = "";
     this.phoneNumber = "";
-    this.id = "";
+    this.key = "";
   }
 
+// Fetches values from newuser.html and pushe them to Firebase to create a new user account
   var user = new UserProfile();
 
-// Fetches values from newuser.html and pushe them to Firebase to create a new user account
-  UserProfile.prototype.createAccount = function() {
+
+  User.prototype.createAccount = function() {
     var newUserURL = new Firebase('https://good-samaritan-cf.firebaseio.com/User');
     newUserURL.push({
       email: $('#new-email').val(),
@@ -28,85 +31,46 @@ $(function() {
   };
 
 
-
-  UserProfile.prototype.renderUserInfo = function(info) {
-    console.log(info);
-  };
-
-  UserProfile.prototype.signIn = function(email, password) {
-    var self = this;
-
+  User.prototype.signIn = function(email, password) {
     var userDataRef = new Firebase("https://good-samaritan-cf.firebaseio.com/User");
+
+    // userDataRef.child("email").on("child_added", function(snapshot) {
+    //   console.log(snapshot.val());
+    // })
 
     userDataRef.orderByChild("email").equalTo(email).on("child_added", function(snapshot) {
       var userObj = snapshot.val();
 
       if (password === userObj.password) {
-        var userData = [];
+        userObj.key = snapshot.key();
 
-        userData.push(snapshot.key());
-        userData.push(userObj.email);
-        userData.push(userObj.password);
-        userData.push(userObj.firstName);
-        userData.push(userObj.lastName);
-        userData.push(userObj.city);
-        userData.push(userObj.state);
-        userData.push(userObj.zip);
-        userData.push(userObj.phoneNumber);
-
-        self.renderUserInfo(userData);
+        sessionStorage.setItem("userStorage", JSON.stringify(userObj));
+        location.href = "userProfile.html";
+      } else {
+        $("#invalid").text("Incorrect password! Please try again.");
       }
+      // } else {
+        // $("p#invalid").text("This account does not exist! Please try again or create a new account.");
+      // }
     })
   }
 
-  function Request() {
-    this.requestorId = "";
-    this.description = "";
-    this.isActive = "";
-    this.date = "";
-  }
-
-  Request.prototype.renderRequestInfo = function() {
-
-  };
-
-// Creates a new request for assistance
-// MISSING jQUERY REFERENCES TO HTML/DOM
-// CLICK EVENT BELOW NEEDS POINTER
-  Request.prototype.createRequest = function() {
-    newUserRequest = new Firebase('https://good-samaritan-cf.firebaseio.com/Request');
-    newUserRequest.push({
-      requestorId: "test id goes here",
-      description: "$('#requestorIDhere').val()",
-      isActive: true,
-      date: event.timeStamp
-    }, console.log("createRequest has been pushed to firebase"))
-  };
-
-// saved sample code for using moment() to return timestamp to viewable date
-/*  $('#test-button').on('click', function(){
-    var dateVar = event.timeStamp;
-    console.log(event.timeStamp);
-    console.log(moment());
-    console.log(moment(1433292533519));
-    console.log(moment(dateVar));
-  })
-*/
-
-
-  Request.prototype.respondRequest = function() {
-
-  };
-
-  Request.prototype.deactivateRequest = function() {
-
-  };
-
-  $("input#sign-in").on("click", function() {
-    var email = $("input#user-email").val();
-    var password = $("input#password").val();
+  $("#sign-in").on("click", function() {
+    var email = $("#user-email").val();
+    var password = $("#password").val();
     user.signIn(email, password);
   })
+
+  $("#password").on("focus", function() {
+    $("#invalid").text("");
+    $("#password").val("");
+  })
+
+  // $("input#user-email").on("focus", function() {
+  //   $("p#invalid").text("");
+  //   $("input#user-email").val("");
+  //   $("input#password").val("");
+  // })
 
   $('#new-user-form').on('submit', user.createAccount);
 
