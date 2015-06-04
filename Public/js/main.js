66ac9fbf992784abe80dc3c354e91f0458040c04
Hollis Lau <hollislau@hotmail.com>
2015-06-04 10:20:22 -0700

    Add rendering of request info to the page and validation of email address

--- Public/js/main.js
+++ Public/js/main.js
@@ -2,13 +2,10 @@ $(function() {
 
   function renderUserInfo() {
 
-      var userObj = JSON.parse(sessionStorage.getItem("userStorage"));
-
       $("#first-name").text("Welcome, " + userObj.firstName + "!");
       $("#location").append(userObj.city + ", " + userObj.state);
       $("#email").append(userObj.email);
       $("#phone-number").append(userObj.phoneNumber);
-      console.log();
   };
 
   function Request() {
@@ -18,8 +15,33 @@ $(function() {
     this.date = "";
   }
 
-  Request.prototype.renderRequestInfo = function() {
+  var userObj = JSON.parse(sessionStorage.getItem("userStorage"));
 
+  var userRequest = new Request();
+
+  Request.prototype.renderRequestInfo = function() {
+    var requestRef = new Firebase("https://good-samaritan-cf.firebaseio.com/Request");
+
+    requestRef.orderByChild("isActive").equalTo(true).on("child_added", function(snapshot) {
+      var activeRequest = snapshot.val();
+
+      if (activeRequest.key === userObj.key) {
+        $("#user-feed").prepend("<p class=" + snapshot.key() + ">" + activeRequest.description + 
+          "</p><button class=" + snapshot.key() + " name=" + userObj.key + ">Deactivate</button>");
+      } else {
+        var userRef = new Firebase("https://good-samaritan-cf.firebaseio.com/User/" + activeRequest.key);
+        
+        userRef.on("value", function(snapshot) {
+          $("#other-feed").prepend("<h4 class=" + snapshot.key() + ">" + snapshot.val().firstName + " " + snapshot.val().lastName +
+            "</h4><p class=" + snapshot.key() + ">" + activeRequest.description +
+            "</p><button class=" + snapshot.key() + " name=" + activeRequest.key + ">Respond</button>");
+        })
+      }
+    })
+
+    requestRef.orderByChild("isActive").equalTo(false).on("child_added", function(snapshot) {
+      $("." + snapshot.key()).remove();
+    })
   };
 
   Request.prototype.createRequest = function() {
@@ -34,7 +56,7 @@ $(function() {
 
   };
 
-
   renderUserInfo();
+  userRequest.renderRequestInfo();
 
 });
